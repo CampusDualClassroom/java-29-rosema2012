b35e30d3da159d6c20b97831b81078a58df89be1
Happy Days!!! <36199346+rosema2012@users.noreply.github.com>
2024-10-01 16:24:04 +0200

    Ejercicio 29 terminado

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -1,7 +1,14 @@
 package com.campusdual.classroom;
 
+import java.io.IOException;
+
 public class Exercise29 {
     public static void main(String[] args) {
+        try {
+            throw new IOException("Exception throwed");
+        } catch (IOException e) {
+            System.out.println("Se ha capturado la IOException: " + e.getMessage());
+        }
 
     }
 }
